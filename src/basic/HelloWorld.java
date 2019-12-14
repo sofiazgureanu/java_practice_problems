0a77511d229145215369bb74852ff95d1a9e3e6e
sofiazgureanu <57046690+sofiazgureanu@users.noreply.github.com>
2019-12-14 13:45:33 -0500

    Update HelloWorld.java

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -3,6 +3,7 @@ package basic;
 public class HelloWorld {
   public static void main(String[] args) throws Exception {
     // Write a Java program to print 'Hello' on screen and then print your name on a separate line.
-
+    System.out.println("Hello");
+    System.out.println("Sofia");
   }
 }
